7b52cfb9d1a1f50732c56eb9f4c08d1f601acacc
ajvergara <ajvergara1014@gmail.com>
2018-09-22 11:49:35 +0300

    Integrated PostCSS & BrowserSync & Completed Desktop Version of Large Hero - next responsive design

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
-const gulp = require("gulp"),
-      watch = require("gulp-watch");
+const gulp         = require("gulp"),
+      watch        = require("gulp-watch"),
+      postcss      = require("gulp-postcss"),
+      autoprefixer = require("autoprefixer"),
+      simpleVars   = require("postcss-simple-vars"),
+      cssImport    = require("postcss-import"),
+      cssNested    = require("postcss-nested"),
+      browserSync  = require("browser-sync").create();
+
+gulp.task("styles", function(){
+  return gulp.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssImport, simpleVars, autoprefixer, cssNested]))
+    .on("error", function(errorInfo){
+      console.log(errorInfo.toString());
+      this.emit("end");
+    })
+    .pipe(gulp.dest("./app/temp/styles/"));
+});
 
 gulp.task("watch", function(){
-  watch("./app/*.html", function(){
-    console.log("HTML has been changed!");
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
   });
 
-  watch("./app/assets/styles/styles.css", function(){
-    console.log("CSS has been changed!");
+  watch("./app/*.html", function(){
+    browserSync.reload();
   });
+
+  watch("./app/assets/styles/**/*.css", function(){
+    gulp.start("cssInject");
+  });
+});
+
+gulp.task("cssInject", ["styles"], function(){
+  return gulp.src("./app/assets/styles/styles.css")
+    .pipe(browserSync.stream());
 });
